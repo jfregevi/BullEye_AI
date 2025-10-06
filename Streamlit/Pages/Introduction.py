ef2169ffeee9831db1384b63c49ff9dd85da5a03
Aleor974 <jean.fregeville@centrale.centralelille.fr>
2025-10-06 20:56:34 +0200

    Update Introduction.py

--- Streamlit/Pages/Introduction.py
+++ Streamlit/Pages/Introduction.py
@@ -112,7 +112,10 @@ def prediction2(ticker, window_size=20, forecast_days=14):
 
     # Préparation des données d'entrée
 
+    X = []
     window_0 = df.iloc[0:window_size][feature_cols].values
+    X.append(window_0)
+    X.append([[]])
     X = np.array([window_0])
 
     X_scaled = np.zeros_like(X)
